df93c345e8abbecb8790a788cfb99be2dadc6c24
ypessoa <yasminspessoa@gmail.com>
2021-07-24 15:10:41 -0500

    header styles

--- src/components/molecules/header/Header.js
+++ src/components/molecules/header/Header.js
@@ -4,10 +4,10 @@ export function Header() {
   return (
     <header className={styles.main}>
       <div className={styles.title}>
-        <h1>then & onwards</h1>
+        <h1>then</h1>
       </div>
       <div className={styles.byline}>
-        by yasmin pessoa
+       onwards
       </div>
     </header>
   );
