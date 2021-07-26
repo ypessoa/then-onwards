61e42c0be52dc02729fcb53f264cc5271cfef15b
ypessoa <yasminspessoa@gmail.com>
2021-07-26 15:14:59 -0500

    add title, favicons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
     <Layout>
       <Helmet>
         <meta charset="UTF-8" />
+        <title>then + onwards</title>
+        <link rel="icon" href="images/favicon/favicon-32.png" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="152x152"
+          href="/images/favicon/favicon-152.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="167x167"
+          href="/images/favicon/favicon-167.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="180x180"
+          href="/images/favicon/favicon-180.png"
+        />
         <meta name="description" content="a pair of interactive images" />
         <meta name="author" content="Yasmin Pessoa" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
